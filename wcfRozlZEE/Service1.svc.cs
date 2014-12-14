907d7d94ae223dd3d5a074ddfdfe804151776611
arek <arek968@gmail.com>
2014-12-14 16:46:38 +0100

    serwis Miasto AJAX

--- wcfRozlZEE/Service1.svc.cs
+++ wcfRozlZEE/Service1.svc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
